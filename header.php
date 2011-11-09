42615dd80a4ba2c981fa8c1d9d8bb1e59acf2963
Kota Miyake <miyake.kota@gmail.com>
2011-11-09 21:44:26 +0900

    html5 for ie fixed

--- header.php
+++ header.php
@@ -10,6 +10,9 @@
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> » RSS" href="<?php bloginfo('rdf_url') ?>">
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> » RSS2" href="<?php bloginfo('rss2_url') ?>">
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> » Atom" href="<?php bloginfo('atom_url') ?>">
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
     <?php wp_head(); ?>
   </head>
   <body>
